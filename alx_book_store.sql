79eebd502fcf884e57d105808231cab41a0d936c
codyempress <codyempress@gmail.com>
2025-08-17 16:45:04 +0300

    Creating Tables

---
+++ alx_book_store.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
+
+CREATE TABLE Books (
+    book_id PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(130), 
+    author_id,
+    price DOUBLE,
+    publication_date DATE,
+
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+    );
+
+CREATE TABLE Authors (
+    author_id PRIMARY KEY,
+    author_name VARCHAR(215)
+    );
+CREATE TABLE Customers (
+    customer_id PRIMARY KEY,
+    customer_name VARCHAR(215)
+    email VARCHAR(215),
+    address TEXT
+    );
+CREATE TABLE Orders (
+    order_id PRIMARY KEY,
+    customer_id,
+    order_date DATE,
+
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details (
+    orderdetailid PRIMARY KEY,
+    order_id,
+    book_id,
+    quantity DOUBLE,
+
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
