a10a1bd6236906d774a04c96bb78ff2d4ec9bf6a
codyempress <codyempress@gmail.com>
2025-08-17 19:28:11 +0300

    Add multiple rows to a table

---
+++ task_6.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer (customer id, customer_name, email, address)
+VALUES
+('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+('Obed Ehobeah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave');
