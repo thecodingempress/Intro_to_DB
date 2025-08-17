6a47226b546728fe9af82cb8a0812248793e8488
codyempress <codyempress@gmail.com>
2025-08-17 19:16:14 +0300

    Add a single entry

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 happiness Ave.')
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
